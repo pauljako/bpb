0a5523e2b01921c0673bd4ce037c36200a82aa38
pauljako <paulifey18@proton.me>
2024-10-19 09:03:59 +0200

    Version: 0.1

--- bpb.py
+++ bpb.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import json
 import sys
+import shutil
 from urllib.request import urlretrieve
 
 def fail(reason: str):
@@ -21,7 +22,7 @@ def report_hook(block_count, block_size, file_size):
         downloaded = size
         print(f"{downloaded}MB/{size}MB ({percentage}%)", end="     \r")
 
-def build(path: str, output: str, should_install: bool, should_compress: bool):
+def build(path: str, output: str | None, should_install: bool, should_compress: bool):
     file_path = os.path.realpath(os.path.join(os.getcwd(), path))
     
     if not os.path.exists(file_path):
@@ -59,6 +60,8 @@ def build(path: str, output: str, should_install: bool, should_compress: bool):
             if not os.path.exists(path):
                 fail(f"Cloning of {path} failed")
                 
+    if os.path.exists(os.path.join(file_path, "package")):
+        shutil.rmtree(os.path.join(file_path, "package"))
     os.mkdir(os.path.join(file_path, "package"))
                 
     print(f"Building {information['name']}")
@@ -67,12 +70,27 @@ def build(path: str, output: str, should_install: bool, should_compress: bool):
         fail("Build failed")
         
     print("Creating boundaries package")
+    os.chdir(os.path.join(file_path, "package"))
+    json_file = information["json_file"]
+    json_file["name"] = information["name"]
+    json_file["version"] = information["version"]
+    
+    with open("boundaries.json", "wt") as f:
+        json.dump(json_file, f)
+        
+    os.chdir(file_path)
+    if output:
+        if os.path.exists(os.path.join(file_path, output)):
+            shutil.rmtree(os.path.join(file_path, output))
+        shutil.move(os.path.join(file_path, "package"), os.path.join(file_path, output))
+        
+    print("Done! Package built")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="bpb", description="The boundaries package builder")
-    parser.add_argument("--install", help="Install the package directly", action="store_true")
-    parser.add_argument("--output", help="The path of the folder/archive to be created")
-    parser.add_argument("--archive", help="Compress the output into a .tar.gz archive", action="store_true")
+    parser.add_argument("--install", "-i", help="Install the package directly", action="store_true")
+    parser.add_argument("--output", "-o", help="The path of the folder/archive to be created", default=None)
+    parser.add_argument("--archive", "-a", help="Compress the output into a .tar.gz archive", action="store_true")
     parser.add_argument("path", help="The path to the archive or folder to build")
     
     args = parser.parse_args()
